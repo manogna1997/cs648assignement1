0cb5d4297944311de8556fcf63447f2c62dd889e
manogna podishetty <abhi@Abhilashs-MBP.san.rr.com>
2020-02-02 10:52:13 -0800

    first commit for cs648

---
+++ src/AboutMe.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+export default class AboutMe extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Manogna Podishetty 2</h1>
+                <img>/Users/abhi/Desktop/gitwork/cs648/cs648assignement1/static/manu.jpeg </img>
+            </div>
+        );
+    }
+}
+
